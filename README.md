241523902002320b77042463077046e0bcc441de
Rudolf Hersén <rudolf.hersen@schibsted.com>
2023-09-22 13:39:56 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# trains-hersen
+
+| site             | repo                 | branch    | tech      |
+| ---------------- | -------------------- | --------- | --------- |
+| trainfreq-hersen | trainfreq            | main      | sveltekit |
+| countdown-hersen | trainident-sveltekit | countdown | sveltekit |
+| text-hersen      | text-sveltekit       | main      | sveltekit |
+| cancelled-hersen | cancelled            | master    | sveltekit |
+| text-vite        | text-vite            | master    | cra       |
+| sheet-react      | text-vite            | sheet     | cra       |
---
+++ src/routes/+page.svelte
@@ -0,0 +1,18 @@
+<a href="https://countdown-hersen.netlify.app/">
+    <img src="countdown.png" width="120" alt="countdown" />
+</a>
+<a href="https://cancelled-hersen.netlify.app">
+    <img src="cancelled.png" width="120" alt="cancelled" />
+</a>
+<a href="https://text-vite.netlify.app/">
+    <img src="text.png" width="120" alt="text" />
+</a>
+<a href="https://sheet-react.netlify.app/">
+    <img src="sheet.png" width="120" alt="sheet" />
+</a>
+<a href="https://text-hersen.netlify.app/">
+    <img src="text.png" width="120" alt="text" />
+</a>
+<a href="https://trainfreq-hersen.netlify.app/">
+    <img src="trainfreq.png" width="120" alt="text" />
+</a>

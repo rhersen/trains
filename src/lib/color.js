368fbb9c07d96f098a232cf238a7f66004e37373
Rudolf Hersén <rudolf.hersen@schibsted.com>
2024-01-05 16:26:35 +0100

    make more trains green

--- src/lib/color.js
+++ src/lib/color.js
@@ -1,13 +1,13 @@
 import { differenceInSeconds, parseISO } from 'date-fns';
 
 export function delay(a) {
-	if (age() > 200) return 'inactive';
+	if (age() > 300) return 'inactive';
 	const delay = seconds();
-	if (delay < 30) return 'delay-0';
-	if (delay < 120) return 'delay-1-min';
-	if (delay < 240) return 'delay-3-min';
-	if (delay < 480) return 'delay-5-min';
-	if (delay < 800) return 'delay-10-min';
+	if (delay < 120) return 'delay-0';
+	if (delay < 180) return 'delay-1-min';
+	if (delay < 300) return 'delay-3-min';
+	if (delay < 600) return 'delay-5-min';
+	if (delay < 900) return 'delay-10-min';
 	return 'delay-15-min';
 
 	function seconds() {

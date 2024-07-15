82b6dfdc4793574cd2b935b05af2d6288d7d22af
Rudolf Hersén <rudolf.hersen@schibsted.com>
2024-07-15 16:50:46 +0200

    new index page for `countdown`

--- src/routes/countdown/+page.svelte
+++ src/routes/countdown/+page.svelte
@@ -1,13 +1,38 @@
-<h1>Välj station</h1>
-<a href="countdown/station/Mr">Märsta</a>
-<a href="countdown/station/Sub">Sundbyberg</a>
-<a href="countdown/station/So">Solna</a>
-<a href="countdown/station/Sod">Odenplan</a>
-<a href="countdown/station/Sci">City</a>
-<a href="countdown/station/Sst">Södra</a>
-<a href="countdown/station/Åbe">Årstaberg</a>
-<a href="countdown/station/Äs">Älvsjö</a>
-<a href="countdown/station/Hu">Huddinge</a>
-<a href="countdown/station/Flb">Flemingsberg</a>
-<a href="countdown/station/Tul">Tullinge</a>
-<a href="countdown/station/Tu">Tumba</a>
+<h1>Pendeltåg</h1>
+<div>
+	<a href="countdown/station/Mr">Märsta</a> •
+	<a href="countdown/station/Upv">Väsby</a> •
+	<a href="countdown/station/So">Solna</a> •
+	<a href="countdown/station/Sod">Odenplan</a> •
+	<a href="countdown/station/Sci">City</a> •
+	<a href="countdown/station/Sst">Södra</a> •
+	<a href="countdown/station/Åbe">Årstaberg</a> •
+	<a href="countdown/station/Äs">Älvsjö</a> •
+	<a href="countdown/station/Hu">Huddinge</a> •
+	<a href="countdown/station/Flb">Flemingsberg</a> •
+	<a href="countdown/station/Tul">Tullinge</a> •
+	<a href="countdown/station/Tu">Tumba</a>
+</div>
+<div>
+	<a href="countdown/station/Sub">Sundbyberg</a>
+</div>
+
+<h1>SJ och Mälartåg</h1>
+<div>
+	<a href="countdown/station/Cst">Centralen</a> •
+	<a href="countdown/station/Nr">Norrköping</a> •
+	<a href="countdown/station/Lu">Lund</a> •
+	<a href="countdown/station/Hpbg">Hallsberg</a> •
+	<a href="countdown/station/Suc">Sundsvall</a>
+</div>
+
+<style>
+	h1,
+	div {
+		font-family: sans-serif;
+	}
+
+	div {
+		font-size: 30px;
+	}
+</style>

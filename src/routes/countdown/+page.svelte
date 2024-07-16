4455618150c7a236bb96714c29c877d474d69fb1
Rudolf Hersén <rudolf.hersen@schibsted.com>
2024-07-16 08:32:06 +0200

    add Mälartåg/snabbtåg pairs

--- src/routes/countdown/+page.svelte
+++ src/routes/countdown/+page.svelte
@@ -9,20 +9,24 @@
 	<a href="countdown/station/Åbe">Årstaberg</a> •
 	<a href="countdown/station/Äs">Älvsjö</a> •
 	<a href="countdown/station/Hu">Huddinge</a> •
-	<a href="countdown/station/Flb">Flemingsberg</a> •
 	<a href="countdown/station/Tul">Tullinge</a> •
 	<a href="countdown/station/Tu">Tumba</a>
 </div>
 <div>
-	<a href="countdown/station/Sub">Sundbyberg</a>
+	<a href="countdown/station/Sub">Sundbyberg</a> •
+	<a href="countdown/station/Flb">Flemingsberg</a>
 </div>
 
 <h1>SJ och Mälartåg</h1>
 <div>
 	<a href="countdown/station/Cst">Centralen</a> •
-	<a href="countdown/station/Nr">Norrköping</a> •
-	<a href="countdown/station/Lu">Lund</a> •
+	<a href="countdown/station/K">Katrineholm</a> •
+	<a href="countdown/station/G">Göteborg</a> •
 	<a href="countdown/station/Hpbg">Hallsberg</a> •
+	<a href="countdown/station/Ksc">Karlstad</a> •
+	<a href="countdown/station/Lp">Linköping</a> •
+	<a href="countdown/station/Lu">Lund</a> •
+	<a href="countdown/station/Gä">Gävle</a> •
 	<a href="countdown/station/Suc">Sundsvall</a>
 </div>
 

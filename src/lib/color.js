27de99e31574f3038035f2b0ec56f0bf6155bfb9
Rudolf Hersén <rudolf.hersen@schibsted.com>
2026-01-16 08:19:25 +0100

    Adjust delay thresholds for more granular timing

--- src/lib/color.js
+++ src/lib/color.js
@@ -3,11 +3,11 @@ import { differenceInSeconds, parseISO } from 'date-fns';
 export function delay(a) {
 	if (age() > 300) return 'inactive';
 	const delay = seconds();
-	if (delay < 120) return 'delay-0';
-	if (delay < 180) return 'delay-1-min';
-	if (delay < 300) return 'delay-3-min';
-	if (delay < 600) return 'delay-5-min';
-	if (delay < 900) return 'delay-10-min';
+	if (delay < 60) return 'delay-0';
+	if (delay < 120) return 'delay-1-min';
+	if (delay < 240) return 'delay-3-min';
+	if (delay < 500) return 'delay-5-min';
+	if (delay < 1000) return 'delay-10-min';
 	return 'delay-15-min';
 
 	function seconds() {
